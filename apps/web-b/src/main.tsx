935127542b3d369ba9d8159ad602d99642524d6e
nari.ku <nari.ku@linecorp.com>
2025-06-07 18:41:08 +0900

    Add greet

--- apps/web-b/src/main.tsx
+++ apps/web-b/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { CMSPage } from './CMSPage';
+import { greet } from "@my/utils";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div className="p-4">
+      <h1>{greet('web-b')}</h1>
       <CMSPage />
     </div>
   </React.StrictMode>
